2813652d5c06fe8afe3ba7bfa5f790bb6c0d1785
Naveen Reddy Dyava <nd2794@columbia.edu>
2024-08-09 22:30:41 -0400

    memory read of data into dataset disabled

--- src/datasets/lasco_datasets.py
+++ src/datasets/lasco_datasets.py
@@ -13,27 +13,28 @@ class lasco_dataset_train(Dataset):
         self.config = config
         self.logger = logger
         self.read_n_process_threads = config["read_n_process_threads"]
-        lasco_data_path = config['data']['lasco']['dir']
-        self.logger.info(f"Loading LASCO-Train dataset from {lasco_data_path}")
+        self.lasco_data_path = config['data']['lasco']['dir']
+        self.logger.info(f"Loading LASCO-Train dataset from {self.lasco_data_path}")
 
         # Image processor & tokenizer
         self.image_processor = AutoProcessor.from_pretrained(config["checkpoint_path"])
         self.tokenizer = AutoTokenizer.from_pretrained(config["checkpoint_path"])
         
-        lasco_json = json.load(open(os.path.join(lasco_data_path, 'lasco_train.json'), 'r'))
+        lasco_json = json.load(open(os.path.join(self.lasco_data_path, 'lasco_train.json'), 'r'))
+        self.triplets = list(map(lambda x: {'query-image': x['query-image'][1], 'target-image': x['target-image'][1], 'query-text': x['query-text']}, lasco_json))
 
+        """
         all_query_images = list(map(lambda x: x['query-image'][1], lasco_json))
         all_target_images = list(map(lambda x: x['target-image'][1], lasco_json))
         all_images = list(set(all_query_images+all_target_images))
 
         self.logger.info("Loading images to memory")
-        all_images_paths_n_names = list(map(lambda x: (os.path.join(lasco_data_path, 'coco', x), x), all_images))
+        all_images_paths_n_names = list(map(lambda x: (os.path.join(self.lasco_data_path, 'coco', x), x), all_images))
         with ThreadPoolExecutor(max_workers=self.read_n_process_threads) as executor:
             results = executor.map(self.load_and_process_image, all_images_paths_n_names)
             self.image_pixvals_map = dict(results)
         self.logger.info("Loading images to memory - Done")
-
-        self.triplets = list(map(lambda x: {'query-image': x['query-image'][1], 'target-image': x['target-image'][1], 'query-text': x['query-text']}, lasco_json))
+        """
 
     def load_and_process_image(self, paths_n_names):
         image = Image.open(paths_n_names[0])
@@ -45,7 +46,12 @@ class lasco_dataset_train(Dataset):
 
     def __getitem__(self, idx):
         triplet = self.triplets[idx]
-        return {'query-image': self.image_pixvals_map[triplet['query-image']], 'target-image': self.image_pixvals_map[triplet['target-image']], 'query-text': triplet['query-text']}
+        #return {'query-image': self.image_pixvals_map[triplet['query-image']], 'target-image': self.image_pixvals_map[triplet['target-image']], 'query-text': triplet['query-text']}
+        return {
+            'query-image': self.load_and_process_image((os.path.join(self.lasco_data_path, 'coco', triplet['query-image']), triplet['query-image']))[1], 
+            'target-image': self.load_and_process_image((os.path.join(self.lasco_data_path, 'coco', triplet['target-image']), triplet['target-image']))[1], 
+            'query-text': triplet['query-text']
+            }
     
     def collate_fn(self, batch):
         query_text = list(map(lambda x: x['query-text'], batch))
@@ -64,27 +70,29 @@ class lasco_dataset_val(Dataset):
         self.config = config
         self.logger = logger
         self.read_n_process_threads = config["read_n_process_threads"]
-        lasco_data_path = config['data']['lasco']['dir']
-        self.logger.info(f"Loading LASCO-Val dataset from {lasco_data_path}")
+        self.lasco_data_path = config['data']['lasco']['dir']
+        self.logger.info(f"Loading LASCO-Val dataset from {self.lasco_data_path}")
 
         # Image processor & tokenizer
         self.image_processor = AutoProcessor.from_pretrained(config["checkpoint_path"])
         self.tokenizer = AutoTokenizer.from_pretrained(config["checkpoint_path"])
         
-        lasco_json = json.load(open(os.path.join(lasco_data_path, 'lasco_val.json'), 'r'))
+        lasco_json = json.load(open(os.path.join(self.lasco_data_path, 'lasco_val.json'), 'r'))
+        self.triplets = list(map(lambda x: {'query-image': x['query-image'][1], 'target-image': x['target-image'][1], 'query-text': x['query-text']}, lasco_json))
 
+        """
         all_query_images = list(map(lambda x: x['query-image'][1], lasco_json))
         all_target_images = list(map(lambda x: x['target-image'][1], lasco_json))
         all_images = list(set(all_query_images+all_target_images))
 
         self.logger.info("Loading images to memory")
-        all_images_paths_n_names = list(map(lambda x: (os.path.join(lasco_data_path, 'coco', x), x), all_images))
+        all_images_paths_n_names = list(map(lambda x: (os.path.join(self.lasco_data_path, 'coco', x), x), all_images))
         with ThreadPoolExecutor(max_workers=self.read_n_process_threads) as executor:
             results = executor.map(self.load_and_process_image, all_images_paths_n_names)
             self.image_pixvals_map = dict(results)
         self.logger.info("Loading images to memory - Done")
+        """
 
-        self.triplets = list(map(lambda x: {'query-image': x['query-image'][1], 'target-image': x['target-image'][1], 'query-text': x['query-text']}, lasco_json))
 
     def load_and_process_image(self, paths_n_names):
         image = Image.open(paths_n_names[0])
@@ -96,7 +104,12 @@ class lasco_dataset_val(Dataset):
 
     def __getitem__(self, idx):
         triplet = self.triplets[idx]
-        return {'query-image': self.image_pixvals_map[triplet['query-image']], 'target-image': self.image_pixvals_map[triplet['target-image']], 'query-text': triplet['query-text']}
+        #return {'query-image': self.image_pixvals_map[triplet['query-image']], 'target-image': self.image_pixvals_map[triplet['target-image']], 'query-text': triplet['query-text']}
+        return {
+            'query-image': self.load_and_process_image((os.path.join(self.lasco_data_path, 'coco', triplet['query-image']), triplet['query-image']))[1], 
+            'target-image': self.load_and_process_image((os.path.join(self.lasco_data_path, 'coco', triplet['target-image']), triplet['target-image']))[1], 
+            'query-text': triplet['query-text']
+            }
     
     def collate_fn(self, batch):
         query_text = list(map(lambda x: x['query-text'], batch))
