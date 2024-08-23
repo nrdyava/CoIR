a81ce78b66136f0bb87121c58aea5c725c03e6de
Naveen Reddy Dyava <nd2794@columbia.edu>
2024-08-23 07:32:01 -0400

    embedder and retriever - datasets and dataloaders successfully tested

--- src/datasets/lasco_datasets.py
+++ src/datasets/lasco_datasets.py
@@ -122,13 +122,23 @@ class lasco_corpus_dataset(Dataset):
 
         if self.config['dataset_split'] == 'train':
             corpus = json.load(open(os.path.join(self.lasco_data_path, 'lasco_train_corpus.json'), 'r'))
-            self.image_book = list(map(lambda x: (int(x[0]), os.path.join(self.lasco_data_path, 'coco', x[1])), list(corpus.items())))
+            self.image_book = list(
+                map(
+                    lambda x: (int(x[0]), os.path.join(self.lasco_data_path, 'coco', x[1])),
+                    list(corpus.items())
+                )
+            )
         elif self.config['dataset_split'] == 'val':
             corpus = json.load(open(os.path.join(self.lasco_data_path, 'lasco_val_corpus.json'), 'r'))
-            self.image_book = list(map(lambda x: (x['id'], os.path.join(self.lasco_data_path, 'coco', 'val2014', x['path'].split('/')[-1])), corpus))
+            self.image_book = list(
+                map(
+                    lambda x: (x['id'], os.path.join(self.lasco_data_path, 'coco', 'val2014', x['path'].split('/')[-1])),
+                    corpus
+                )
+            )
         else:
             lasco_splits = {'train': 1, 'val': 2}
-            print(lasco_splits[config['dataset_split']])
+            print(lasco_splits[self.config['dataset_split']])
 
         # Image processor & tokenizer
         self.image_processor = AutoProcessor.from_pretrained(config["checkpoint_path"])
@@ -153,3 +163,81 @@ class lasco_corpus_dataset(Dataset):
         image_key = list(map(lambda x: x['image-key'], batch))
         image = {'pixel_values': torch.cat(list(map(lambda x: x['image']['pixel_values'], batch)), dim=0)}
         return {'image-key': image_key, 'image': image}
+
+
+
+class lasco_retrieval_dataset(Dataset):
+    def __init__(self, config):
+        self.config = config
+        self.lasco_data_path = config['data']['lasco']['dir']
+
+        if self.config['dataset_split'] == 'train':
+            lasco_json = json.load(open(os.path.join(self.lasco_data_path, 'lasco_train.json'), 'r'))
+            self.triplets = list(
+                map(
+                    lambda x: {
+                        'query-image-id': x['query-image'][0],
+                        'query-image': x['query-image'][1],
+                        'target-image-id': x['target-image'][0],
+                        'target-image': x['target-image'][1],
+                        'query-text': x['query-text']
+                    },
+                    lasco_json
+                )
+            )
+        elif self.config['dataset_split'] == 'val':
+            lasco_json = json.load(open(os.path.join(self.lasco_data_path, 'lasco_val.json'), 'r'))
+            self.triplets = list(
+                map(
+                    lambda x: {
+                        'query-image-id': x['query-image'][0],
+                        'query-image': x['query-image'][1],
+                        'target-image-id': x['target-image'][0],
+                        'target-image': x['target-image'][1],
+                        'query-text': x['query-text']
+                    },
+                    lasco_json
+                )
+            )
+        else:
+            lasco_splits = {'train': 1, 'val': 2}
+            print(lasco_splits[self.config['dataset_split']])
+
+        # Image processor & tokenizer
+        self.image_processor = AutoProcessor.from_pretrained(config["checkpoint_path"])
+        self.tokenizer = AutoTokenizer.from_pretrained(config["checkpoint_path"])
+
+    def load_and_process_image(self, img_path):
+        image = Image.open(img_path)
+        processed_image = self.image_processor(images=image, return_tensors="pt")
+        return processed_image
+
+    def __len__(self):
+        return len(self.triplets)
+
+    def __getitem__(self, idx):
+        triplet = self.triplets[idx]
+        return {
+            'query-image-id': triplet['query-image-id'],
+            'query-image': self.load_and_process_image(os.path.join(self.lasco_data_path, 'coco', triplet['query-image'])),
+            'target-image-id': triplet['target-image-id'],
+            'target-image': self.load_and_process_image(os.path.join(self.lasco_data_path, 'coco', triplet['target-image'])),
+            'query-text': triplet['query-text']
+        }
+
+    def collate_fn(self, batch):
+        query_text = list(map(lambda x: x['query-text'], batch))
+        query_text = self.tokenizer(query_text, padding=True, return_tensors="pt")
+
+        query_image_ids = list(map(lambda x: x['query-image-id'], batch))
+        target_image_ids = list(map(lambda x: x['target-image-id'], batch))
+        query_image = {'pixel_values': torch.cat(list(map(lambda x: x['query-image']['pixel_values'], batch)), dim=0)}
+        target_image = {'pixel_values': torch.cat(list(map(lambda x: x['target-image']['pixel_values'], batch)), dim=0)}
+
+        return {
+            'query-image-id': query_image_ids,
+            'query-image': query_image,
+            'target-image-id': target_image_ids,
+            'target-image': target_image,
+            'query-text': query_text
+        }
