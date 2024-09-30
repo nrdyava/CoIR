fff9d03023b02df9367c7a256af21fb260ada252
Naveen Reddy Dyava <nd2794@columbia.edu>
2024-09-30 05:34:24 -0400

    clamping temperature

--- src/losses/contrastive_loss_with_softmax.py
+++ src/losses/contrastive_loss_with_softmax.py
@@ -4,8 +4,8 @@ def loss_with_temp_rank(embeds_1, embeds_2, logit_scale):
     logits_per_A = torch.mm(embeds_1, embeds_2.t())
     diagonal_elements = torch.diag(logits_per_A)
     avg_rank = torch.sum(logits_per_A >= diagonal_elements.unsqueeze(1), dim = 1).float().mean()
-    logits_per_A = logits_per_A*logit_scale.exp()
-    logits_per_B = torch.mm(embeds_2, embeds_1.t())*logit_scale.exp()
+    logits_per_A = logits_per_A*torch.clamp(logit_scale.exp(), max=100)
+    logits_per_B = torch.mm(embeds_2, embeds_1.t())*torch.clamp(logit_scale.exp(), max=100)
     
     labels = torch.arange(logits_per_A.size(0), device=logits_per_A.device)
 
