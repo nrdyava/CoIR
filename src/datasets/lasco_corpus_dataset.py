730af33a6d354e04fb9b28767b47b22a3e9f081e
Naveen Reddy Dyava <nd2794@columbia.edu>
2024-10-11 10:57:30 -0400

    rewriting corpus and retrieval datasets

--- src/datasets/lasco_corpus_dataset.py
+++ src/datasets/lasco_corpus_dataset.py
@@ -6,41 +6,45 @@ import json
 from transformers import AutoProcessor, AutoTokenizer
 from torch.utils.data import DataLoader, Dataset
 
-class lasco_corpus_dataset(Dataset):
-    def __init__(self, retriever_config, checkpoint_config):
-        self.retriever_config = retriever_config
-        self.checkpoint_config = checkpoint_config
-        self.lasco_data_path = self.retriever_config['data']['lasco']['dir']
+class lasco_corpus_dataset_clip(Dataset):
+    def __init__(self, dataset_split, lasco_data_path, clip_ckpt_path, device):
+        """
+        Args:
+            dataset_split (string): options: ['train', 'val']
+            lasco_data_path (string): path to the lasco dataset
+            clip_ckpt_path (string): path to the clip checkpoint
+        """
 
-        if self.retriever_config['dataset_split'] == 'train':
-            corpus = json.load(open(os.path.join(self.lasco_data_path, 'metadata', 'lasco_train_corpus.json'), 'r'))
+        if dataset_split == 'train':
+            corpus = json.load(open(os.path.join(lasco_data_path, 'metadata', 'lasco_train_corpus.json'), 'r'))
             self.image_book = list(
                 map(
                     lambda x: {
                         'image-key': x['image-id'], 
-                        'image': os.path.join(self.lasco_data_path, x['image-path'])
+                        'image': os.path.join(lasco_data_path, x['image-path'])
                         },
                     corpus
                     )
                 )
-        elif self.retriever_config['dataset_split'] == 'val':
-            corpus = json.load(open(os.path.join(self.lasco_data_path, 'metadata', 'lasco_val_corpus.json'), 'r'))
+            
+        elif dataset_split == 'val':
+            corpus = json.load(open(os.path.join(lasco_data_path, 'metadata', 'lasco_val_corpus.json'), 'r'))
             self.image_book = list(
                 map(
                     lambda x: {
                         'image-key': x['image-id'], 
-                        'image': os.path.join(self.lasco_data_path, x['image-path'])
+                        'image': os.path.join(lasco_data_path, x['image-path'])
                         },
                     corpus
                     )
                 )
+            
         else:
-            lasco_splits = {'train': 1, 'val': 2}
-            print(lasco_splits[self.retriever_config['dataset_split']])
+            print('Invalid dataset split. Please choose from [train, val]')
 
         # Image processor & tokenizer
-        self.image_processor = AutoProcessor.from_pretrained(self.retriever_config['clip_checkpoints'][self.checkpoint_config['model_type']])
-        self.tokenizer = AutoTokenizer.from_pretrained(self.retriever_config['clip_checkpoints'][self.checkpoint_config['model_type']])
+        self.image_processor = AutoProcessor.from_pretrained(clip_ckpt_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(clip_ckpt_path)
 
     def load_and_process_image(self, img_path):
         image = Image.open(img_path)
@@ -51,10 +55,9 @@ class lasco_corpus_dataset(Dataset):
         return len(self.image_book)
 
     def __getitem__(self, idx):
-        image_tuple = self.image_book[idx]
         return {
-            'image-key': image_tuple[0],
-            'image': self.load_and_process_image(image_tuple[1])
+            'image-key': self.image_book[idx]['image-key'],
+            'image': self.load_and_process_image(self.image_book[idx]['image'])
         }
 
     def collate_fn(self, batch):
