1f6f4f16f30f3f70345e10c998b05505c1a22461
Naveen Reddy Dyava <nd2794@columbia.edu>
2024-11-06 06:17:01 -0500

    machine generated data - vllm and llama-3.1

---
+++ machine_data_gen/machine-instruction-generation.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Import Libraries
+
+# In[1]:
+
+
+from vllm import LLM, SamplingParams
+import tqdm as notebook_tqdm
+import json
+import os
+import re
+import copy
+from huggingface_hub import login
+
+with open('/home/nd2794/keys.json', 'r') as file:
+    keys = json.load(file)
+
+cache_dir = '/proj/vondrick4/naveen/HF_CACHE_DIR'
+hf_token = keys["HF_READ_TOKEN"]
+login(token=hf_token)
+
+os.environ['TRANSFORMERS_CACHE'] = cache_dir
+os.environ['HUGGINGFACE_HUB_CACHE'] = cache_dir
+os.environ['PYTORCH_TRANSFORMERS_CACHE'] = cache_dir
+os.environ['PYTORCH_PRETRAINED_BERT_CACHE'] = cache_dir
+os.environ['HF_HUB_CACHE'] = cache_dir
+
+
+# ## Define Utility Functions
+
+# In[2]:
+
+
+def get_instruction(source_event, target_event):
+    common_instruct = """Two images (Source & Target) are described by their captions.
+Your have generate a query text for composed image retrieval task that describes how the Source image should be modified to get the Target image.
+Each image may multiple captions which are listed in a new line. You must read all the captions for each image to fully understand the image.
+Only output the query text."""
+
+    source_instruct = """SOURCE IMAGE CAPTIONS:\n""" + "\n".join(f"- {item}" for item in source_event)
+
+    target_instruct = """TARGET IMAGE CAPTIONS:\n""" + "\n".join(f"- {item}" for item in target_event)
+
+    return common_instruct + '\n\n' + source_instruct + '\n\n' + target_instruct
+
+
+# In[3]:
+
+
+ex_source_cap = [
+    'A person in blue and orange shirt riding a bicycle.',
+    'A bicyclist riding in front of speed blurred foliage. ',
+    'A cyclist with a helmet bicycles quickly on a road.',
+    'The man is riding his bike down the street. '
+]
+
+ex_target_cap = [
+    'A man is riding a motorcycle down the street',
+    'A man riding a motorcycle down a street next to a restaurant.',
+    'Helmeted motorcyclist riding on roadway in populated setting. ',
+    'A motorcycle going down a street very fast.'
+]
+
+ex_query = "The person is riding a motorcycle"
+
+
+# In[4]:
+
+
+conversation_template = [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant"
+    },
+    {
+        "role": "user",
+        "content": get_instruction(ex_source_cap, ex_target_cap)
+    },
+    {
+        "role": "assistant",
+        "content": ex_query 
+    }
+]
+
+
+# ## Load Model
+
+# In[5]:
+
+
+model_name = 'meta-llama/Meta-Llama-3.1-8B-Instruct'
+llm = LLM(
+    model=model_name, 
+    tensor_parallel_size=2, 
+    download_dir=cache_dir,
+    dtype='bfloat16'
+)
+
+
+# In[7]:
+
+
+sampling_params = SamplingParams(
+    #temperature=1.0,
+    max_tokens=200,
+)
+
+
+# ## Load the data
+
+# In[44]:
+
+
+split = 'train'
+if split == 'train':
+    data = json.load(open('/proj/vondrick4/naveen/coir-data/LaSCo/metadata/lasco_train_indexed_MT(img_cap).json', 'r'))
+    imgs_and_caps = json.load(open('/proj/vondrick4/naveen/coir-data/annotations/images_and_caps_train2014_processed_dict.json', 'r'))
+elif split == 'val':
+    data = json.load(open('/proj/vondrick4/naveen/coir-data/LaSCo/metadata/lasco_val_indexed_MT(img_cap).json', 'r'))
+    imgs_and_caps= json.load(open('/proj/vondrick4/naveen/coir-data/annotations/images_and_caps_val2014_processed_dict.json', 'r'))
+    for key in imgs_and_caps.keys():
+        imgs_and_caps[key] = [imgs_and_caps[key]]
+
+
+# ## Process the data
+
+# In[45]:
+
+
+conversations = []
+tracker = {}
+i = 0
+
+for sample in data:
+    tracker[sample['id']] = {}
+    
+    conversation_forward = copy.deepcopy(conversation_template)
+    conversation_forward.append(
+        {
+            "role": "user",
+            "content": get_instruction(imgs_and_caps[str(sample["coir"]["query-image-id"])], imgs_and_caps[str(sample["coir"]["target-image-id"])])
+        }
+    )
+    
+    conversation_reverse = copy.deepcopy(conversation_template)
+    conversation_reverse.append(
+        {
+            "role": "user",
+            "content": get_instruction(imgs_and_caps[str(sample["coir"]["target-image-id"])], imgs_and_caps[str(sample["coir"]["query-image-id"])])
+        }
+    )
+    
+    conversations.append(conversation_forward)
+    tracker[sample['id']]['forward'] = i
+    i+=1
+    
+    conversations.append(conversation_reverse)
+    tracker[sample['id']]['reverse'] = i
+    i+=1
+
+
+# ## Run vLLM
+
+# In[46]:
+
+
+outputs = llm.chat(
+    messages=conversations,
+    sampling_params=sampling_params,
+    use_tqdm=True
+)
+
+
+# ## Process & Write Outputs
+
+# In[47]:
+
+
+processed_outputs = []
+for out in outputs:
+    processed_outputs.append(out.outputs[0].text)
+
+
+# In[48]:
+
+
+for sample in data:
+    sample["coir"]['query-text-forward-mg'] = processed_outputs[tracker[sample['id']]['forward']]
+    sample["coir"]['query-text-reverse-mg'] = processed_outputs[tracker[sample['id']]['reverse']]
+
+
+# In[49]:
+
+
+with open('/proj/vondrick4/naveen/coir-data/LaSCo/metadata/lasco_{}_indexed_MT(img_cap)_MG.json'.format(split), 'w') as json_file:
+    json.dump(data, json_file, indent=4)
+
---
+++ retrieval/clip_3en_MT_AF_SO_GT_QN/lasco.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+device = 0
+exp_name = '2024-11-05-23-15-03-245492 + clip_inbatch_3en_MT_AF_SO_GT_QN_LR_1e-5'
+batch_size=64
+
+out_dir = '/proj/vondrick4/naveen/coir-ret-results'
+runs_dir = '/proj/vondrick4/naveen/coir-runs'
+dataset_split = 'val'
+lasco_data_path = '/local/vondrick/naveen/coir-data/LaSCo'
+device_map = 'cuda:{}'.format(device)
+
+
+# In[2]:
+
+
+clip_checkpoints = {
+    'CLIP-ViT-B/16': '/local/vondrick/naveen/pretrained_models/clip/clip-vit-base-patch16',
+    'CLIP-ViT-B/32': '/local/vondrick/naveen/pretrained_models/clip/clip-vit-base-patch32',
+    'CLIP-ViT-L/14': '/local/vondrick/naveen/pretrained_models/clip/clip-vit-large-patch14',
+    'CLIP-ViT-L/14@336': '/local/vondrick/naveen/pretrained_models/clip/clip-vit-large-patch14-336'
+}
+
+
+# In[3]:
+
+
+import sys
+sys.path.append('/proj/vondrick4/naveen/CoIR')
+import warnings
+warnings.filterwarnings("ignore")
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import CLIPVisionModelWithProjection, CLIPTextModelWithProjection
+import faiss
+import torch
+import numpy as np
+import os
+import json
+import pandas as pd
+
+from src.datasets.lasco_corpus_dataset import lasco_corpus_dataset_clip
+from src.datasets.lasco_retrieval_dataset import lasco_retrieval_dataset_clip
+from src.metrics.metrics import calculate_recall
+from src.models.clip.inbatch_3en_MT_AF_SO_GT_QN import CLIPModel
+
+
+
+
+exp_path = os.path.join(runs_dir, exp_name)
+checkpoints = sorted(list(filter(lambda x: x[:10] == 'checkpoint', os.listdir(exp_path))))
+os.makedirs(os.path.join(out_dir, exp_name), exist_ok=True)
+
+
+
+
+print('Evaluating Experiment: {}'.format(exp_name))
+
+exp_results = pd.DataFrame(columns=['checkpoint', 'epoch', 'Recall@1', 'Recall@5', 'Recall@10', 'Recall@50', 'Recall@100', 'Recall@500', 'Recall@1000'])
+exp_results.to_csv(os.path.join(out_dir, exp_name, 'experiment_results.csv'), index=False)
+
+for checkpoint in checkpoints:
+    checkpoint_path = os.path.join(runs_dir, exp_name, checkpoint)
+
+    print('Evaluating checkpoint: {}'.format(checkpoint))
+    model = CLIPModel.load_from_checkpoint(
+        checkpoint_path = checkpoint_path, 
+        map_location=device_map,
+        strict=False
+    )
+    model.eval()
+    print('Model loaded')
+
+    clip_checkpoint_path = clip_checkpoints[model.config['model_type']]
+    print('Using device: {}'.format(device_map))
+
+    d = model.image_encoder.config.projection_dim
+    index = faiss.IndexFlatIP(d)
+
+    res = faiss.StandardGpuResources()
+    index = faiss.index_cpu_to_gpu(res, device, index)
+
+    corpus_dataset = lasco_corpus_dataset_clip(dataset_split, lasco_data_path, clip_checkpoint_path)
+    corpus_dataloader = DataLoader(
+        dataset=corpus_dataset,
+        collate_fn=corpus_dataset.collate_fn,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=10,
+        pin_memory=True,
+        drop_last=False,
+        persistent_workers=True
+    )
+
+    retrieval_dataset = lasco_retrieval_dataset_clip(dataset_split, lasco_data_path, clip_checkpoint_path)
+    retrieval_dataloader = DataLoader(
+        dataset=retrieval_dataset,
+        collate_fn=retrieval_dataset.collate_fn,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=10,
+        pin_memory=True,
+        drop_last=False,
+        persistent_workers=True
+    )
+
+    index_cntr = 0
+    index_id_to_image_id_map = {}
+
+    for batch_idx, batch in enumerate(tqdm(corpus_dataloader, desc="Indexing Corpus")):
+        batch['image']['pixel_values'] = batch['image']['pixel_values'].to(device_map)
+        with torch.no_grad():
+            image_embeds = model.img_forward(batch)
+        index.add(image_embeds['image-embeds'].cpu())
+
+        batch_len = len(batch['image-key'])
+        batch_start_indx = index_cntr
+        batch_end_indx = batch_start_indx + batch_len
+
+        for key, value in zip(list(range(batch_start_indx, batch_end_indx)), batch['image-key']):
+            index_id_to_image_id_map[key] = value
+        index_cntr += batch_len
+
+    map_func = np.vectorize(lambda x: index_id_to_image_id_map[x])
+
+    output = []
+
+    for batch_idx, batch in enumerate(tqdm(retrieval_dataloader , desc="Retrieval Task")):
+        with torch.no_grad():
+            batch['query-image']['pixel_values'] = batch['query-image']['pixel_values'].to(device_map)
+            batch['query-text']['input_ids'] = batch['query-text']['input_ids'].to(device_map)
+            batch['query-text']['attention_mask'] = batch['query-text']['attention_mask'].to(device_map)
+        
+            target_hat_embeds = model.img_txt_forward(batch)
+
+        D, I = index.search(target_hat_embeds['target-hat-embeds'].cpu(), k=1000)
+        I = map_func(I)
+
+        batch_size = len(batch['query-image-id'])
+        for i in range(batch_size):
+            output.append({
+                'id': batch['id'][i],
+                'query-image-id': batch['query-image-id'][i],
+                'target-image-id': batch['target-image-id'][i],
+                'query-text-raw': batch['query-text-raw'][i],
+                'top_1000_ret_cands': I[i][:].tolist(),
+                'top_1000_ret_cands_cos_sims': D[i][:].tolist()
+            })
+
+    with open(os.path.join(out_dir, exp_name, 'outputs'+'-'+checkpoint+'.json'), "w") as json_file:
+        json.dump(output, json_file, indent=4)
+
+    metrics = []
+    ground_truths = np.array(list(map(lambda x: x['target-image-id'], output)))
+    retrieved_candidates = np.array(list(map(lambda x: x['top_1000_ret_cands'], output)))
+
+    Recall_1 = 100*calculate_recall(ground_truths, retrieved_candidates, 1)
+    Recall_5 = 100*calculate_recall(ground_truths, retrieved_candidates, 5)
+    Recall_10 = 100*calculate_recall(ground_truths, retrieved_candidates, 10)
+    Recall_50 = 100*calculate_recall(ground_truths, retrieved_candidates, 50)
+    Recall_100 = 100*calculate_recall(ground_truths, retrieved_candidates, 100)
+    Recall_500 = 100*calculate_recall(ground_truths, retrieved_candidates, 500)
+    Recall_1000 = 100*calculate_recall(ground_truths, retrieved_candidates, 1000)
+    metrics.append({"Recall@1": Recall_1})
+    metrics.append({"Recall@5": Recall_5})
+    metrics.append({"Recall@10": Recall_10})
+    metrics.append({"Recall@50": Recall_50})
+    metrics.append({"Recall@100": Recall_100})
+    metrics.append({"Recall@500": Recall_500})
+    metrics.append({"Recall@1000": Recall_1000})
+
+    with open(os.path.join(out_dir, exp_name, 'metrics'+'-'+checkpoint+'.json'), "w") as json_file:
+        json.dump(metrics, json_file, indent=4)
+
+    epoch = int(checkpoint[17:20])
+    new_row = pd.DataFrame([{
+        'checkpoint': checkpoint, 
+        'epoch': epoch, 
+        'Recall@1': Recall_1,
+        'Recall@5': Recall_5,
+        'Recall@10': Recall_10,
+        'Recall@50': Recall_50,
+        'Recall@100': Recall_100,
+        'Recall@500': Recall_500,
+        'Recall@1000': Recall_1000
+    }])
+    exp_results = pd.concat([exp_results, new_row], ignore_index=True)
+    exp_results.to_csv(os.path.join(out_dir, exp_name, 'experiment_results.csv'), index=False)
+
+
+
